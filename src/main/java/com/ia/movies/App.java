d9e275f215d314e78a65dca20fbbd9965ed4bba5
Axel Rodriguez <axel.rodriguez@cemaco.com>
2020-05-05 21:31:05 -0600

    Carga la data desde mongodb

--- src/main/java/com/ia/movies/App.java
+++ src/main/java/com/ia/movies/App.java
@@ -2,9 +2,21 @@ package com.ia.movies;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import com.google.gson.Gson;
+
+import com.ia.movies.clases.Movie;
+import java.util.List;
+import java.util.ArrayList;
+
 
 /**
  * Hello world!
@@ -12,14 +24,39 @@ import javafx.stage.Stage;
  */
 public class App extends Application
 {
+    VBox contenedor_peliculas = new VBox();
+    MongoClient mongo_client = null;
+
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setTitle("Recomendacion de peliculas");
+        List<Movie> peliculas = getMovies();
+        System.out.println(peliculas.get(0).toString());
+        Scene scene = new Scene(new StackPane(), 720, 480);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public List<Movie> getMovies() {
+        List<Movie> peliculas = new ArrayList<Movie>();
+        mongo_client = new MongoClient("localhost", 27017);
+        MongoDatabase database = mongo_client.getDatabase("movie_metadata");
+        MongoCollection<Document> collection = database.getCollection("movies");
+        MongoCursor<Document> cursor = collection.find().iterator();
+        System.out.println("paso esto");
+        try {
+            while (cursor.hasNext()) {
+                String result_string = cursor.next().toJson().toString();
+                Gson json_result = new Gson();
+                Movie pelicula = json_result.fromJson(result_string, Movie.class);
+                peliculas.add(pelicula);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            cursor.close();
+        }
+        return peliculas;
     }
 
     public static void main( String[] args )
---
+++ src/main/java/com/ia/movies/clases/Movie.java
@@ -0,0 +1,34 @@
+package com.ia.movies.clases;
+
+import org.bson.types.ObjectId;
+
+public class Movie {
+
+    public ObjectId _id;
+
+    public String color;
+
+    public String director;
+
+    public String[] actores;
+
+    public String[] keywords;
+
+    public String[] genres;
+
+    public String rating;
+
+    public String language;
+
+    public int year;
+
+    public String title;
+
+    public float score;
+
+    public int facebook_likes;
+    
+    public int duration;
+
+    public String country;
+}
